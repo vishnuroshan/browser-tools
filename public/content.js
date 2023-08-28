583ecbcb8002339dd7124a8ceeb1a229647bfa0a
vishnu <vishnu@sedintechnologies.com>
2023-08-28 17:30:24 +0530

    changes

---
+++ public/content.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+browser.runtime.onMessage.addListener(async function (message, _sender, sendResponse) {
+  if (message?.type === 'start-wl') {
+    let watchListRemoval = setInterval(function () {
+      document.querySelector('#primary button[aria-label="Action menu"]')?.click()
+      const things = document.evaluate(
+        '//span[contains(text(),"Remove from")]',
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null,
+      )
+
+      for (let i = 0; i < things.snapshotLength; i++) {
+        things?.snapshotItem(i)?.click()
+      }
+    }, 1000)
+    sendResponse(watchListRemoval)
+  } else if (typeof message.type === 'number') {
+    if (message?.type) {
+      console.log(message.type)
+      clearInterval(message.type)
+    }
+    sendResponse('end')
+  }
+})

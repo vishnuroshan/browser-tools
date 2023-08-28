0b358a55f6d8afb734fc925ff0118a4fd7ad95a9
vishnu <vishnu@sedintechnologies.com>
2023-08-28 17:43:35 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
+import { useState, useEffect } from "react"
+
 function App() {
+  const [watchLater, setWatchLater] = useState<boolean>(false)
+  const [intervalId, setIntervalId] = useState<number>(0)
+  const [tab, setTab] = useState<browser.tabs.Tab>();
+  const getType = (wl: boolean) => (wl ? 'start-wl' : intervalId)
+
+  const getCurrentTab = async () => {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true })
+    setTab(tabs[0])
+  }
+
+  const isYoutube = (url: string): boolean => url === 'https://www.youtube.com/playlist?list=WL'
+
+  useEffect(() => {
+    getCurrentTab()
+  }, [])
+
+  const handleClick = async () => {
+    if (tab && tab.id) {
+      const response = await browser.tabs.sendMessage<{ type: string | number | null }, number | string>(tab.id, { type: getType(!watchLater) })
+      if (typeof response === "number") {
+        setIntervalId(response)
+      }
+      setWatchLater(!watchLater)
+    }
+  }
+
   return (
-    <div className="min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <input id="wl-chbx" type="checkbox"></input><label htmlFor="wl-chbx">sdsdsds</label>
-    </div>
-  );
+    <main>
+      <h3>Browser tools</h3>
+      {tab && tab.id && tab.url && <div className="container">
+        <div className={`field ${isYoutube(tab.url) ? '' : 'disabled-field'}`}>
+          <input
+            disabled={!isYoutube(tab.url)}
+            checked={watchLater}
+            onClick={handleClick}
+            type="checkbox"
+            id="switch"
+          />
+          <label htmlFor="switch">{watchLater ? 'stop' : 'start'} watch later removal</label>
+        </div>
+      </div>}
+    </main>
+  )
 }
 
 export default App;

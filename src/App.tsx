58eb8a5a2d70d11bd82846695e3afca26d6d829d
vishnu <vishnu@sedintechnologies.com>
2023-08-28 17:52:50 +0530

    UI complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-
+import './App.css';
 function App() {
   const [watchLater, setWatchLater] = useState<boolean>(false)
   const [intervalId, setIntervalId] = useState<number>(0)
